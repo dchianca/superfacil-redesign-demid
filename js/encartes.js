110d7c2387bc97a378a90aa074c95749a41b4c42
dchianca <danilochianca@gmail.com>
2026-07-12 20:13:04 -0300

    Add files via upload

---
+++ js/encartes.js
@@ -0,0 +1,77 @@
+const encartesPorLoja = {
+
+    "SuperFácil Emaús": [
+        {
+            nome: "Férias",
+            validade: "Válido até 12/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        },
+        {
+            nome: "Dia de Feira",
+            validade: "Válido até 12/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil Rodoviária": [
+        {
+            nome: "Festival do Café",
+            validade: "Válido até 20/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil São Gonçalo": [
+        {
+            nome: "Ofertas da Semana",
+            validade: "Válido até 18/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil Moema Tinoco": [
+        {
+            nome: "Férias",
+            validade: "Válido até 12/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil Vale do Sol": [
+        {
+            nome: "Hortifruti",
+            validade: "Válido até 14/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil Nova Betânia": [
+        {
+            nome: "Festival da Carne",
+            validade: "Válido até 22/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil Alto de São Manoel": [
+        {
+            nome: "Dia de Feira",
+            validade: "Válido até 12/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ],
+
+    "SuperFácil João Pessoa": [
+        {
+            nome: "Arraiá de Ofertas",
+            validade: "Válido até 25/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        },
+        {
+            nome: "Hortifruti",
+            validade: "Válido até 14/07/2026",
+            pdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        }
+    ]
+
+};
